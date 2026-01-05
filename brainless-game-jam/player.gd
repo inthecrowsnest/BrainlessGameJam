2dd8ab685c2486c0c1f838bdde0c5d2c78b4a7ea
Ellie <elidalt06@gmail.com>
2026-01-05 14:26:37 -0500

    basic player movement and shooting
    added simple asteroids style movement (rotation and forward/back) as well as shooting a simple projectile (bullet.tscn) via pressing the shoot key (currently space bar).
    
    does not yet utilize physics for acceleration on player movement, so is still a little stiff, but playable. will consider adding more physics based movement for polish :)?

--- brainless-game-jam/player.gd
+++ brainless-game-jam/player.gd
@@ -1,14 +1,40 @@
 extends CharacterBody2D
 
-# BASIC MOVEMENT SCRIPT FOR TESTING DIALOGUE BOX - plans to replace with state machine
-# straight ripped from the godot tutorial docs lol bc i was too lazy to find my old code to reuse
+#movement taken from godot intro to 2d tutorial
 # https://docs.godotengine.org/en/stable/tutorials/2d/2d_movement.html
+
+# shooting taken from this kidscancode.org godot recipe webpage
+# https://kidscancode.org/godot_recipes/4.x/2d/2d_shooting/index.html
+
+@onready var bullet_timer = %BulletDelay
+
 @export var speed = 400
+@export var rotation_speed = 1.5
+@export var bullet : PackedScene
+@export var fire_delay : float = 0.2
+
+var rotation_direction = 0
+var is_shooting = false 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		is_shooting = true
+	elif event.is_action_released("shoot"):
+		is_shooting = false
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	rotation_direction = Input.get_axis("left", "right")
+	velocity = transform.x * Input.get_axis("down", "up") * speed
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	get_input()
+	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
+	if is_shooting and bullet_timer.is_stopped():
+		bullet_timer.start(fire_delay)
+		shoot()
+	
+func shoot():
+	var b = bullet.instantiate()
+	owner.add_child(b)
+	b.transform = %BulletSpawnPoint.global_transform
