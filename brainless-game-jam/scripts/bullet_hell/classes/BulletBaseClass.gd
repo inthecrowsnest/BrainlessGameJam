ae92b03775819b4088026b9ad9c096ec133d664f
Jackfulll <jacobo15773@gmail.com>
2026-01-05 16:10:48 -0600

    Created enemy and bullet classes and the initial prototype of the bullet hell section

---
+++ brainless-game-jam/scripts/bullet_hell/classes/BulletBaseClass.gd
@@ -0,0 +1,14 @@
+# BulletBaseClass.gd 
+extends Area2D
+class_name Bullet
+
+@export var BulletDataFile : BulletData
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print('A bullet entered scene')
+	
+
+
+func setup_bullet_data(data):
+	BulletDataFile = data 
---
+++ brainless-game-jam/scripts/bullet_hell/classes/EnemyDataClass.gd
@@ -0,0 +1,13 @@
+# EnemyData.gd - Resource to fill basic enemy information/stats
+extends Resource
+class_name EnemyData
+
+# This will contain interchangeable data to make different enemies
+@export var id : String
+@export var name : String
+@export var health : int = 100
+@export var speed : float = 30.0
+@export var texture : Texture # We can switch enemy sprites with this
+
+# We can attach a script to this variable to create various enemy types with different behaviours: 
+@export var enemy_mechanics : Resource 
