27ddc4cd2199da2df269895ae72c72226fa56a8b
Jackfulll <jacobo15773@gmail.com>
2026-01-07 16:46:52 -0600

    Designed a simple ui
    Left some space to maybe try to fit compact version of bullet hell (?)

--- brainless-game-jam/scripts/level_loader.gd
+++ brainless-game-jam/scripts/level_loader.gd
@@ -1,15 +1,17 @@
 # level_loader.gd
-# This node will be in every level and will be in charge of loading & unloading levels.
+# This node will be in every level and will be in charge of loading levels.
 extends Node
 
 #temp ----
 @export var next_level : PackedScene 
-# IDEALLY: next_level = Global.level[number_of_level] or 
-@onready var level = get_parent().current_level
+# IDEALLY: next_level = Global.levels[number_of_level] or \
+# @onready var level = get_parent().current_level
 
 #temp ----
 
-
+func _ready() -> void:
+	pass
+	#load_level(next_level)?
 
 func load_level(level):
 	get_tree().change_scene_to_packed(level)
